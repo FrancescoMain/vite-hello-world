983a02ab448fa4a279554cde622ba2c9d385ca35
Francesco <cesaranofrancescomain@gmail.com>
2022-12-07 18:33:25 +0100

    creato componente img

---
+++ src/components/appHero.vue
@@ -0,0 +1,22 @@
+<script>
+export default {
+    data() {
+        return {
+            img: "./public/img/jumbo.png",
+        };
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <img :src="img" alt="jumbo">
+    </div>
+</template>
+
+<style scoped>
+img {
+    max-width: 100%;
+}
+</style>
