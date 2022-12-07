fc180d1deddf3f8c2880e06f68f29766ec59fcee
Francesco <cesaranofrancescomain@gmail.com>
2022-12-07 18:25:38 +0100

    Creato Componente h1 che stampa un msg

---
+++ src/components/appTitle.vue
@@ -0,0 +1,20 @@
+<script>
+export default {
+    data() {
+        return {
+            msg: "La mia prima app con Vite!",
+        };
+    }
+}
+
+</script>
+
+<template>
+    <div>
+        <h1>{{ msg }}</h1>
+    </div>
+</template>
+
+<style scoped>
+
+</style>
